3f9b83a975dd647e2d50ff5285b570f735cc489a
mattyocode <matthewoliver@live.co.uk>
2021-07-28 18:16:58 +0100

    snapshot added to tests

--- src/components/navigation/MainNavigation.test.js
+++ src/components/navigation/MainNavigation.test.js
@@ -2,13 +2,22 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./index";
-import { linksData } from "../../fixtures/navData";
+// import { linksData } from "../../fixtures/navData";
 
 // jest.mock("react-router-dom", () => {});
 
 describe("<Navbar /> tests", () => {
-  it("renders the <Navbar/> component", async () => {
-    const { getByTestId } = render(
+  const linksData = [
+    {
+      id: 1,
+      url: "/",
+      text: "Browse All",
+      highlight: false,
+    },
+  ];
+
+  it("renders the <Navbar/> component", () => {
+    const { container, getByTestId, getByText } = render(
       <Router>
         <Navbar>
           <Navbar.Header>
@@ -21,23 +30,7 @@ describe("<Navbar /> tests", () => {
     );
     expect(getByTestId("navigation")).toBeTruthy();
     expect(getByTestId("toggle-btn")).toBeTruthy();
+    expect(getByText("browse all", { exact: false })).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
-
-// it("renders the <Navbar/> component", () => {
-//   render(<Navbar />);
-
-//   expect(screen.getByTestId("navigation")).not.toBeNull();
-// });
-
-// it("renders link to 'all movies'", () => {
-//   render(<Navbar />);
-
-//   expect(screen.getByText("Browse All")).not.toBeNull();
-// });
-
-// it("toggle button changes visibility in state", () => {
-//   render(<Navbar />);
-
-//   expect(screen.getByTestId("toggle-btn")).toBeTruthy();
-// });
