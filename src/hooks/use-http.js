d68eabdb8313943eae4a3a634afc0379d7045c73
mattyocode <matthewoliver@live.co.uk>
2021-08-10 18:03:32 +0100

    refactor http request function into custom hook

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,6 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
-const useHttp = (config, dataFn) => {
-  const [isLoading, setIsLoading] = useState(true);
+export default function useHttp(config, applyData) {
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-};
+
+  const sendRequest = useCallback(async (config) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(config.url, {
+        method: config.method ? config.method : "GET",
+        headers: config.headers ? config.headers : {},
+        body: config.body ? JSON.stringify(config.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed");
+      }
+
+      const data = await response.json();
+
+      applyData(data);
+    } catch (err) {
+      setError(err);
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+}
