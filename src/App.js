0dd2ac627d9e29f0f5719866c923df73d3884b0e
Matt <63904102+mattyocode@users.noreply.github.com>
2021-11-02 12:19:04 +0000

    fix ReactGA issues - using Router instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, { Suspense } from "react";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
-import { createBrowserHistory } from "history";
+import React, { Suspense, useEffect } from "react";
+import { Router, Redirect, Route, Switch, useHistory } from "react-router-dom";
 import ReactGA from "react-ga";
 import { Home, MovieDetail, Movies } from "./pages";
 import { Loading } from "./components";
 import { NavbarContainer } from "./containers/navigation";
 
 export default function App() {
-  const history = createBrowserHistory();
-
-  history.listen((location) => {
-    ReactGA.initialize("G-ZXD4BK35ZX");
-    ReactGA.set({ page: location.pathname });
-    ReactGA.pageview(location.pathname);
-  });
+  const history = useHistory();
+  useEffect(() => {
+    history.listen((location) => {
+      let path = location.pathname;
+      if (location.hash) {
+        path = location.pathname + location.hash;
+      }
+      ReactGA.initialize("G-ZXD4BK35ZX");
+      ReactGA.set({ page: path });
+      ReactGA.pageview(path);
+    });
+  }, [history]);
 
   return (
     <Suspense fallback={<Loading />}>
@@ -28,8 +27,8 @@ export default function App() {
           <Route path="/" exact>
             <Home />
           </Route>
-          <Route path="/movies" exact>
-            <Movies />
+          <Route path="/movies" exact component={Movies}>
+            {/* <Movies /> */}
           </Route>
           <Route path="/movies/:movieId">
             <MovieDetail />
